32c52c7aa96752f96084ddbde92eeca684ec04fc
Lucas Van de Mosselaer <lucvdm5@gmail.com>
2024-07-08 15:22:14 -0600

    Updated plugin info, reorganized

--- PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace APWOL
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "APWOL";
-        public const string PLUGIN_NAME = "APWOL";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
