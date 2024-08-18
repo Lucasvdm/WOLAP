9adde57c34e843fe6deb7164bd3b6ecf749a3413
Lucas Van de Mosselaer <lucvdm5@gmail.com>
2024-08-17 21:25:57 -0600

    Patched dialog window to add some toggleable debug info back in

---
+++ src/DebugDialogPatches.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WOLAP
+{
+    [HarmonyPatch]
+    internal class DebugDialogPatches
+    {
+        private static KeyCode TOGGLE_DIALOG_DEBUG = KeyCode.Backslash;
+
+        [HarmonyPatch(typeof(Dialog), "Update")]
+        [HarmonyPrefix]
+        static void DialogUpdatePatch(Dialog __instance)
+        {
+            if (Controls.dev.GetKeycodeDown(TOGGLE_DIALOG_DEBUG))
+            {
+                Traverse traverse = Traverse.Create(__instance);
+                GameObject rootObject = traverse.Field("debugObjectsRoot").GetValue<GameObject>();
+
+                WolapPlugin.Log.LogInfo($"Attempting to toggle dialog debug features! Root Object isNull: {rootObject == null}");
+
+                if (rootObject != null)
+                {
+                    bool active = !rootObject.activeSelf;
+                    SetDialogDebugInfoVisible(rootObject, active);
+                    WolapPlugin.Log.LogInfo($"Toggling dialog debug features! Debug active: {active}");
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Dialog), "SetDebugObjectsVisible")]
+        [HarmonyPrefix]
+        static bool SetDialogDebugObjectsVisiblePatch(Dialog __instance, bool fVisible)
+        {
+            Traverse traverse = Traverse.Create(__instance);
+            GameObject rootObject = traverse.Field("debugObjectsRoot").GetValue<GameObject>();
+
+            //ONLY do the SetActive call if the fVisible parameter is false -- if SetDebugObjectsVisible is called with 'true' (in Update), ignore it.
+            if (rootObject != null && !fVisible) rootObject.SetActive(false); 
+
+            return false;
+        }
+
+        //Handling the real toggle in this method instead of SetDialogDebugObjectsVisible because there's an annoying call to that method in Update which always sets it to true
+        static void SetDialogDebugInfoVisible(GameObject debugInfoRoot, bool visible)
+        {
+            debugInfoRoot.transform.position = debugInfoRoot.transform.position.WithY(0f);
+            debugInfoRoot.SetActive(visible);
+        }
+    }
+}
