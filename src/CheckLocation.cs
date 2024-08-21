a74af8816fb33779f258b319245a8bb4d178d82d
Lucas Van de Mosselaer <lucvdm5@gmail.com>
2024-08-21 16:55:47 -0600

    Starting to add code for patching checks into the JSON data

---
+++ src/CheckLocation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WOLAP
+{
+    internal class CheckLocation
+    {
+        public string Name { get; set; }
+        public MCommand ScriptCommand { get; set; }
+        public List<ScriptPath> JsonCheckPaths { get; set; }
+        public List<ScriptPath> JsonMissPaths { get; set; }
+
+        public CheckLocation(string name, List<ScriptPath> checkPaths, List<ScriptPath> missPaths) { 
+            this.Name = name;
+            this.ScriptCommand = new MCommand(MCommand.Op.STATESHARE, [name]); //STATESHARE is a Stadia-exclusive command which has no use, seems like the best dangling MCommand.Op value to hijack
+            this.JsonCheckPaths = checkPaths;
+            this.JsonMissPaths = missPaths;
+        }
+    }
+
+    public struct ScriptPath
+    {
+        public string scriptName;
+        public string stateName;
+        public int index;
+
+        public ScriptPath(string script, string state, int index)
+        {
+            this.scriptName = script;
+            this.stateName = state;
+            this.index = index;
+        }
+    }
+}
---
+++ src/CheckLocationManager.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WOLAP
+{
+    internal class CheckLocationManager
+    {
+        public List<CheckLocation> LocationList { get; set; }
+
+        public CheckLocationManager()
+        {
+
+        }
+
+        public CheckLocation GetLocation(string locName)
+        {
+            if (locName.IsNullOrWhiteSpace()) return null;
+
+            return LocationList.Find(loc => loc.Name == locName);
+        }
+
+        public void InsertCheckPickupCommands()
+        {
+            Dictionary<string, MScript> scripts = ModelManager.Instance.scripts;
+            foreach (CheckLocation location in LocationList)
+            {
+                foreach (ScriptPath path in location.JsonCheckPaths)
+                {
+                    List<MCommand> stateCommands = scripts[path.scriptName].states[path.stateName].commands;
+                    stateCommands.RemoveAt(path.index);
+                    stateCommands.Insert(path.index, location.ScriptCommand);
+                }
+            }
+        }
+
+        public void InsertCheckMissCommands()
+        {
+
+        }
+    }
+}
