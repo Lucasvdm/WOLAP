1151516389c04585ce8d04c128eb73652f62b6c4
Lucas Van de Mosselaer <lucvdm5@gmail.com>
2024-08-23 11:06:53 -0600

    Patched StringToOp to define checklocation command that resolves to Op.STATESHARE

---
+++ src/MCommandPatches.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+namespace WOLAP
+{
+    [HarmonyPatch]
+    internal class MCommandPatches
+    {
+        [HarmonyPatch(typeof(MCommand), "StringToOp")]
+        [HarmonyPostfix]
+        static void StringToOpPatch(string s, ref MCommand.Op __result)
+        {
+            if (s == "checklocation") __result = MCommand.Op.STATESHARE;
+        }
+    }
+}
