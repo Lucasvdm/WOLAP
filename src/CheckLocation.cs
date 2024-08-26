e270c2cbff153dc3ba6c8f7f6aeff0dbc2bac3d7
Lucas Van de Mosselaer <lucvdm5@gmail.com>
2024-08-26 16:38:04 -0600

    Removed WIP code for path-based JSON data modification and removed unmodified states from custom JSON script data

--- src/CheckLocation.cs
+++ src/CheckLocation.cs
@@ -8,30 +8,12 @@ namespace WOLAP
     {
         public string Name { get; set; }
         public MCommand ScriptCommand { get; set; }
-        public ScriptPath[] JsonCheckPaths { get; set; }
-        public ScriptPath[] JsonMissPaths { get; set; }
         public bool IsDLC { get; set; }
 
-        public CheckLocation(string name, ScriptPath[] checkPaths, ScriptPath[] missPaths, bool isDlc) { 
+        public CheckLocation(string name, bool isDlc) { 
             this.Name = name;
             this.ScriptCommand = new MCommand(MCommand.Op.STATESHARE, new[]{name}); //STATESHARE is a Stadia-exclusive command which has no use, seems like the best dangling MCommand.Op value to hijack
-            this.JsonCheckPaths = checkPaths;
-            this.JsonMissPaths = missPaths;
             this.IsDLC = isDlc;
-        }
-    }
-
-    public struct ScriptPath
-    {
-        public string scriptName;
-        public string stateName;
-        public int index;
-
-        public ScriptPath(string script, string state, int index)
-        {
-            this.scriptName = script;
-            this.stateName = state;
-            this.index = index;
         }
     }
 }
