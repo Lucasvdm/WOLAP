bd65f23758e60af5ceac4251ee588d7c2f7a400c
Lucas Van de Mosselaer <lucvdm5@gmail.com>
2024-10-30 17:50:59 -0600

    Fixed namespace

--- src/ArchipelagoItem.cs
+++ src/ArchipelagoItem.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace WOLAP.src
+namespace WOLAP
 {
     internal class ArchipelagoItem
     {
--- src/ArchipelagoItemManager.cs
+++ src/ArchipelagoItemManager.cs
@@ -4,7 +4,7 @@ using System.Text;
 using UnityEngine.XR;
 using UnityEngine;
 
-namespace WOLAP.src
+namespace WOLAP
 {
     internal class ArchipelagoItemManager
     {
