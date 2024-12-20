d0dcd0920e786190f78333c986f2588b7508a32b
Lucas Van de Mosselaer <lucvdm5@gmail.com>
2024-12-20 14:57:51 -0700

    Deleted ModelManagerPatches, moved contents to SavedGamePatches (to be renamed)

--- src/SavedGamePatches.cs
+++ src/SavedGamePatches.cs
@@ -4,16 +4,17 @@ using System.IO;
 using System.Text;
 using HarmonyLib;
 using System.Reflection;
+using System.Reflection.Emit;
 using UnityEngine.SceneManagement;
 
 namespace WOLAP
 {
-    [HarmonyPatch(typeof(SavedGame))]
+    [HarmonyPatch]
     internal class SavedGamePatches
     {
         private static bool ModDataLoaded = false;
 
-        [HarmonyPatch("NewGame")]
+        [HarmonyPatch(typeof(SavedGame), "NewGame")]
         [HarmonyPostfix]
         private static void NewGamePatch(string strNameFirst, string strNameLast, string strNameFull, bool fIsCowgirl, int meatReward, string strAnimal, ref string __result)
         {
@@ -69,5 +70,45 @@ namespace WOLAP
                 WolapPlugin.Log.LogInfo("Custom JSON data parsed and merged.");
             }
         }
+
+        [HarmonyPatch(typeof(ModelManager), "ParseScripts")]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> ParseScriptsTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var insts = new List<CodeInstruction>(instructions);
+
+            //Potentially brittle search conditions, but I doubt this game's code will ever be updated significantly, and compatibility with other mods is not a priority for this project
+            int ifOverwriteIdx = insts.FindIndex(inst => inst.Is(OpCodes.Ldfld, m_fOverwriteOk));
+            if (ifOverwriteIdx == -1)
+            {
+                WolapPlugin.Log.LogError("Failed to transpile ParseScripts! Could not find the m_fOverwriteOk check.");
+                return instructions;
+            }
+
+            int loadTextIdx = insts.FindIndex(ifOverwriteIdx, inst => inst.opcode == OpCodes.Ldloc_1);
+            if (loadTextIdx == -1)
+            {
+                WolapPlugin.Log.LogError("Failed to transpile ParseScripts! Could not find the ldloc.1 load.");
+                return instructions;
+            }
+
+            insts.RemoveAt(loadTextIdx); //Remove load for local variable "text", don't need it
+            int assignmentIdx = insts.FindIndex(loadTextIdx, inst => inst.opcode == OpCodes.Callvirt);
+            if (assignmentIdx == -1)
+            {
+                WolapPlugin.Log.LogError("Failed to transpile ParseScripts! Could not find the set_Item callvirt.");
+                return instructions;
+            }
+
+            insts.RemoveAt(assignmentIdx); //Remove call to set_Item assignment (this.scripts[text] = mscript)
+
+            //Insert instruction to call InjectedHelpers.OverwriteScriptStates(this.scripts, mscript);
+            insts.Insert(assignmentIdx, new CodeInstruction(OpCodes.Call, m_OverwriteScriptStates));
+
+            return insts;
+        }
+
+        static MethodInfo m_OverwriteScriptStates = SymbolExtensions.GetMethodInfo(() => InjectedHelpers.OverwriteScriptStates(null, null));
+        static FieldInfo m_fOverwriteOk = typeof(ModelManager).GetField("m_fOverwriteOk", BindingFlags.NonPublic | BindingFlags.Instance);
     }
 }
