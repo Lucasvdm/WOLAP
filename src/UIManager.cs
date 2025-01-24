338e0e8b8bed0cd2a7e8016e2c38c29ebc4955b1
Lucas Van de Mosselaer <lucvdm5@gmail.com>
2025-01-23 22:20:50 -0700

    Code and asset tweaks for setting up archipelago connection settings UI box (visually -- not yet functional). Also the UI setup happens on/before the title screen now instead of waiting for the gameplay state, and item-received box is hidden and disabled after being created (and then enabled/fully disabled again whenever it fades in/out).

--- src/UIManager.cs
+++ src/UIManager.cs
@@ -15,6 +15,7 @@ namespace WOLAP
         public ConcurrentQueue<ReceivedID> RcvItemQueue { get; private set; }
 
         private bool isUiModified;
+
         private InventoryItem rcvItemCard;
         private CanvasGroup rcvItemWindowCG;
         private CanvasGroup rcvItemItemCG;
@@ -23,6 +24,8 @@ namespace WOLAP
         private float rcvItemShowTime = 2.5f;
         private bool rcvItemFading;
         private bool rcvQueueInProgress;
+
+        private CanvasGroup apConnectionBox;
 
         private void Awake()
         {
@@ -46,14 +49,22 @@ namespace WOLAP
 
         private bool IsReadyToModifyUI()
         {
-            return UI.instance != null && WolapPlugin.Assets != null && WestOfLoathing.instance.state_machine.IsState(GameplayState.NAME);
+            return UI.instance != null && WolapPlugin.Assets != null;
         }
-
-        //TODO: Define sizing using a HorizontalLayoutGroup or something on the parent frame instead of setting anchors in code, which would also allow for resizing the frame to fit longer/shorter item names
+        
         private void SetUpUIChanges()
         {
             WolapPlugin.Log.LogInfo("Setting up new UI elements.");
 
+            CreateRcvItemWindow();
+            CreateAPConnectionBox();
+
+            WolapPlugin.Log.LogInfo("Finished setting up new UI elements.");
+        }
+
+        //TODO: Define sizing using a HorizontalLayoutGroup or something on the parent frame instead of setting anchors in code, which would also allow for resizing the frame to fit longer/shorter item names
+        private void CreateRcvItemWindow()
+        {
             var ropeFrame = WolapPlugin.Assets.LoadAsset<GameObject>(Constants.PluginAssetsPath + "/ui/popup frame.prefab");
 
             var receiptBox = Instantiate(ropeFrame);
@@ -61,9 +72,9 @@ namespace WOLAP
             receiptBox.transform.SetParent(UI.instance.GetComponentInParent<Canvas>().transform, false);
             rcvItemWindowCG = receiptBox.GetComponent<CanvasGroup>();
             rcvItemWindowCG.alpha = 0f;
+            rcvItemWindowCG.gameObject.SetActive(false);
 
             var inventory = AssetBundleManager.Load<Inventory>("prefabs/ui/inventory.prefab");
-            WolapPlugin.Log.LogInfo("Inventory: " + inventory.gameObject);
             rcvItemCard = Instantiate<InventoryItem>(inventory.itemPrefab);
             rcvItemCard.GetComponent<Button>().onClick.RemoveAllListeners();
             rcvItemCard.style = Inventory.Style.TwoColumn;
@@ -83,8 +94,96 @@ namespace WOLAP
             rcvTextRect.anchorMin = new Vector2(0.05f, 0.05f);
             rcvTextRect.anchorMax = new Vector2(0.28f, 0.95f);
             rcvTextRect.sizeDelta = Vector2.zero;
+        }
 
-            WolapPlugin.Log.LogInfo("Finished setting up new UI elements.");
+        private void CreateAPConnectionBox()
+        {
+            WolapPlugin.Log.LogDebug("Setting up Archipelago connection settings window");
+
+            var ropeFrame = WolapPlugin.Assets.LoadAsset<GameObject>(Constants.PluginAssetsPath + "/ui/ap connection box.prefab");
+
+            WolapPlugin.Log.LogDebug("Setting up rope frame");
+            var apConnectionGO = Instantiate(ropeFrame);
+            AttachRopeKnotScripts(apConnectionGO);
+            apConnectionGO.transform.SetParent(UI.instance.GetComponentInParent<Canvas>().transform, false);
+            apConnectionBox = apConnectionGO.GetComponent<CanvasGroup>();
+
+            WolapPlugin.Log.LogDebug("Loading dialog prompt");
+            var dialogPrompt = AssetBundleManager.Load<DialogPrompt>("prefabs/ui/dialog_prompt_switch.prefab"); //dialog_prompt_switch has a WolInputField - dialog_prompt only has InputField for some reason
+
+            var content = apConnectionBox.transform.Find("Content");
+
+            WolapPlugin.Log.LogDebug("Setting up title row");
+            var titleRow = content.Find("Title Row");
+            var titleText = titleRow.gameObject.AddComponent<WolText>();
+            titleText.text = "Archipelago Settings";
+            titleText.color = Color.black;
+            titleText.fontStyle = FontStyle.Bold;
+            titleText.alignment = TextAnchor.MiddleCenter;
+            titleText.resizeTextForBestFit = true;
+            titleText.resizeTextMaxSize = 32;
+
+            WolapPlugin.Log.LogDebug("Setting up first input row");
+            var inputRows = content.Find("Input Rows");
+            var firstRow = inputRows.GetChild(0);
+            var firstLabelText = firstRow.GetChild(0).gameObject.AddComponent<WolText>();
+            firstLabelText.text = "Slot:";
+            firstLabelText.color = Color.black;
+            firstLabelText.fontStyle = FontStyle.Bold;
+            firstLabelText.alignment = TextAnchor.MiddleRight;
+            firstLabelText.resizeTextForBestFit = true;
+            firstLabelText.resizeTextMaxSize = 28;
+            var dpInput = dialogPrompt.transform.Find("window/input");
+            var inputCopy = Instantiate(dpInput, firstRow.GetChild(1).transform, false);
+            var inputCopyRect = inputCopy as RectTransform;
+            inputCopyRect.anchorMin = Vector2.zero;
+            inputCopyRect.anchorMax = Vector2.one;
+            var firstInput = firstRow.GetComponentInChildren<WolInputField>();
+            ((Text)firstInput.placeholder).text = "";
+            firstInput.characterLimit = 100;
+
+            WolapPlugin.Log.LogDebug("Setting up input row copies");
+            for (int i = 0; i < 3; i++)
+            {
+                var newRow = Instantiate(firstRow, inputRows, false);
+                var newLabel = newRow.GetChild(0).GetComponent<WolText>();
+                var newInput = newRow.GetChild(1).GetComponentInChildren<WolInputField>();
+
+                switch (i)
+                {
+                    case 0:
+                        newLabel.text = "Host:";
+                        newInput.text = "archipelago.gg";
+                        ((Text)newInput.placeholder).text = "archipelago.gg";
+                        break;
+                    case 1:
+                        newLabel.text = "Port:";
+                        break;
+                    case 2:
+                        newLabel.text = "Password:";
+                        break;
+                }
+            }
+
+            WolapPlugin.Log.LogDebug("Setting up connection row + button");
+            var connRow = content.Find("Connection Row");
+            var buttonContainer = connRow.GetChild(0);
+            var wolButton = dialogPrompt.GetComponentInChildren<Button>();
+            var newButton = Instantiate(wolButton, buttonContainer, false);
+            var buttonText = newButton.GetComponentInChildren<WolText>();
+            buttonText.text = "Connect";
+            newButton.onClick.RemoveAllListeners();
+            var buttonRect = newButton.transform as RectTransform;
+            buttonRect.anchorMin = Vector2.zero;
+            buttonRect.anchorMax = Vector2.one;
+
+            WolapPlugin.Log.LogDebug("Setting up connection row text");
+            var connText = connRow.GetChild(1).gameObject.AddComponent<WolText>();
+            connText.text = "Not Connected";
+            connText.color = Color.red;
+            connText.alignment = TextAnchor.MiddleCenter;
+            connText.resizeTextForBestFit = true;
+            connText.resizeTextMaxSize = 22;
         }
 
         private void AttachRopeKnotScripts(GameObject ropeFrame)
@@ -146,12 +245,14 @@ namespace WOLAP
                 if (cg.alpha < 0f) cg.alpha = 0f;
                 yield return null;
             }
+            cg.gameObject.SetActive(false);
             rcvItemFading = false;
         }
 
         private IEnumerator FadeInCanvasGroup(CanvasGroup cg, float fadeTimeInSeconds)
         {
             rcvItemFading = true;
+            cg.gameObject.SetActive(true);
             while (cg.alpha < 1f)
             {
                 cg.alpha += Time.deltaTime / fadeTimeInSeconds;
