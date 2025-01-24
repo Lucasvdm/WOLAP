ce0011f4b77f89cc77d0a935eaaba83e4128436a
Lucas Van de Mosselaer <lucvdm5@gmail.com>
2025-01-23 22:21:27 -0700

    Added OnEnable to RopeKnot script so it actually happens automatically

--- src/Component Scripts/RopeKnot.cs
+++ src/Component Scripts/RopeKnot.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class RopeKnot : MonoBehaviour
 {
+    private void OnEnable()
+    {
+        RotateRandomly();
+    }
+
     public void RotateRandomly()
     {
         transform.localEulerAngles = new Vector3(0f, 0f, Random.Range(0, 3) * 90f);
